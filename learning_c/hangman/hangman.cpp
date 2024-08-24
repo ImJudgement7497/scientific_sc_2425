363910f2aa0b146fe6d853c52debb26515a77c3a
ImJudgement7497 <bmjm501@york.ac.uk>
2024-08-24 20:10:34 +0100

    save

--- learning_c/hangman/hangman.cpp
+++ learning_c/hangman/hangman.cpp
@@ -2,135 +2,146 @@
 #include <random>
 #include <vector>
 #include <fstream>
-#include <algorithm> 
-#include <cctype>    
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
-struct Play {
+struct Play
+{
 
-    string goal; // goal word for the game
-    string unknown; // Gets replaced with letters as they are guessed (starts as "___")
+    string goal;          // goal word for the game
+    string unknown;       // Gets replaced with letters as they are guessed (starts as "___")
     vector<char> letters; // Capital Alphabet
-    int score = 0; // Score
-
+    int score = 0;        // Score
 };
 
 // ASCI art for hangman
-void print_hangman(int stage) {
-    switch (stage) {
-        case 0:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 1:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 2:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "   |     |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 3:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "  /|     |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 4:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "  /|\\    |\n";
-            cout << "         |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 5:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "  /|\\    |\n";
-            cout << "  /      |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        case 6:
-            cout << "   -------\n";
-            cout << "   |     |\n";
-            cout << "   O     |\n";
-            cout << "  /|\\    |\n";
-            cout << "  / \\    |\n";
-            cout << "         |\n";
-            cout << "----------\n";
-            break;
-        default:
-            cout << "Invalid stage\n";
+void print_hangman(int stage)
+{
+    switch (stage)
+    {
+    case 0:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 1:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 2:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "   |     |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 3:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "  /|     |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 4:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "  /|\\    |\n";
+        cout << "         |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 5:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "  /|\\    |\n";
+        cout << "  /      |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    case 6:
+        cout << "   -------\n";
+        cout << "   |     |\n";
+        cout << "   O     |\n";
+        cout << "  /|\\    |\n";
+        cout << "  / \\    |\n";
+        cout << "         |\n";
+        cout << "----------\n";
+        break;
+    default:
+        cout << "Invalid stage\n";
     }
 }
 
 // Pass a string by constant reference and make it all lowercase
-void to_lower_case(string& str) {
-    transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {
-        return tolower(c);
-    });
+void to_lower_case(string &str)
+{
+    transform(str.begin(), str.end(), str.begin(), [](unsigned char c)
+              { return tolower(c); });
 }
 
 // Pass a char by reference and make it lowercase
-void to_lower_case(char& c) {
+void to_lower_case(char &c)
+{
     c = tolower(static_cast<unsigned char>(c));
 }
 
 // Pass by constant reference
-void print_vector(const vector<int>& v) {
-    for (int num : v) {
+void print_vector(const vector<int> &v)
+{
+    for (int num : v)
+    {
         cout << num << ' ';
     }
     cout << endl;
 }
 
 // Pass by constant reference
-void print_vector(const vector<char>& v) {
-    for (char n : v) {
+void print_vector(const vector<char> &v)
+{
+    for (char n : v)
+    {
         cout << n << ' ';
     }
     cout << endl;
 }
 
 // Pass by reference so we modify the original structure, not a copy
-void get_random_word(Play& current_play) {
+void get_random_word(Play &current_play)
+{
 
-    // Load file into a vector called lines 
+    // Load file into a vector called lines
     vector<string> lines;
     string line;
     ifstream inputFile("1000-most-common-words.txt");
 
     lines.reserve(1000);
 
-    if (inputFile) {
-        while (getline(inputFile, line)) {
+    if (inputFile)
+    {
+        while (getline(inputFile, line))
+        {
             lines.push_back(line);
         }
     }
-    else {
+    else
+    {
         cout << "File could not be opened" << endl;
     }
 
@@ -144,30 +155,33 @@ void get_random_word(Play& current_play) {
     uniform_int_distribution<> dis(0, lines.size() - 1);
 
     current_play.goal = lines[dis(gen)];
-
 }
 
 // Ask for a user input and modify original structure
-void get_string_from_user(Play& current_play) {
+void get_string_from_user(Play &current_play)
+{
 
     cout << "Enter a phrase: ";
     getline(cin >> ws, current_play.goal);
-
 }
 
 // Initalise the guess with "____"
-int initalise_guess(Play& current_play) {
+int initalise_guess(Play &current_play)
+{
 
     int length_change = 0;
 
-    for (int i = 0; i < current_play.goal.size(); i++) {
-        if (current_play.goal[i] == ' ') {
+    for (int i = 0; i < current_play.goal.size(); i++)
+    {
+        if (current_play.goal[i] == ' ')
+        {
 
             current_play.unknown = current_play.unknown + " ";
             length_change++;
         }
 
-        else {
+        else
+        {
 
             current_play.unknown = current_play.unknown + "_";
         }
@@ -176,44 +190,51 @@ int initalise_guess(Play& current_play) {
 }
 
 // Initalise the alphabet
-void initialise_alphabet(Play& current_play) {
+void initialise_alphabet(Play &current_play)
+{
 
     current_play.letters.clear();
     // Add uppercase alphabet letters to the vector
-    for (char letter = 'a'; letter <= 'z'; ++letter) {
+    for (char letter = 'a'; letter <= 'z'; ++letter)
+    {
         current_play.letters.push_back(letter);
     }
 }
 
 // Inistalise the goal string through random or user input, and the mapping
-void get_inital_info(Play& current_play) {
+void get_inital_info(Play &current_play)
+{
 
     bool stop = true;
-    while (stop) {
+    while (stop)
+    {
 
         string start_string;
 
         cout << "Would you like a random word (Y) or to enter your own phrase (N): " << endl;
         cin >> start_string;
 
-        if (start_string == "N") {
+        if (start_string == "N")
+        {
             get_string_from_user(current_play);
             to_lower_case(current_play.goal);
             stop = false;
         }
-        else if (start_string == "Y") {
+        else if (start_string == "Y")
+        {
             get_random_word(current_play);
             stop = false;
         }
-        else {
+        else
+        {
             cout << "Error: Please answer Y or N" << endl;
         }
     }
-
 }
 
 // Get guess from user and assign it to the reference of a pointer
-char* get_guesses(char* guess_ptr){
+char *get_guesses(char *guess_ptr)
+{
 
     guess_ptr = new char; // Allocate memory for a new string
     cout << "Please enter your guess: " << endl;
@@ -224,30 +245,36 @@ char* get_guesses(char* guess_ptr){
 }
 
 // Function to check guess
-vector<int>* check_guess(Play& current_play, const char* guess_ptr) {
+vector<int> *check_guess(Play &current_play, const char *guess_ptr)
+{
     char current_guess = *guess_ptr;
-    vector<int>* indice_ptr = new vector<int>;
+    vector<int> *indice_ptr = new vector<int>;
 
-    for (int i = 0; i < current_play.goal.size(); i++) {
-        if (current_guess == current_play.goal[i]) {
+    for (int i = 0; i < current_play.goal.size(); i++)
+    {
+        if (current_guess == current_play.goal[i])
+        {
             indice_ptr->push_back(i);
         }
     }
 
-    if (indice_ptr -> empty()) {
+    if (indice_ptr->empty())
+    {
         delete indice_ptr;
         return nullptr;
     }
-    
+
     return indice_ptr;
 }
 
 // Change the letters in unknown to the guessed letter through the indice pointer
-int change_unknown(Play& current_play, char* guess_ptr, vector<int>* indice_ptr) {
-    
+int change_unknown(Play &current_play, char *guess_ptr, vector<int> *indice_ptr)
+{
+
     int score = 0;
 
-    for (int i : *indice_ptr) {
+    for (int i : *indice_ptr)
+    {
         current_play.unknown[i] = *guess_ptr;
         score++;
     }
@@ -256,17 +283,20 @@ int change_unknown(Play& current_play, char* guess_ptr, vector<int>* indice_ptr)
 }
 
 // Change letters in alphabet to empty character
-void remove_letters(Play& current_play, char* guess_ptr) {
+void remove_letters(Play &current_play, char *guess_ptr)
+{
 
-    for (int i = 0; i < current_play.letters.size(); i++) {
-        if (current_play.letters[i] == *guess_ptr) {
+    for (int i = 0; i < current_play.letters.size(); i++)
+    {
+        if (current_play.letters[i] == *guess_ptr)
+        {
             current_play.letters[i] = ' ';
         }
     }
-
 }
 // Function to start the game
-void start_game() {
+void start_game()
+{
     Play play;
     cout << "WELCOME TO HANGMAN" << endl;
 
@@ -276,23 +306,27 @@ void start_game() {
 
     bool stop = true;
     int life = 0;
-    char* guess_ptr = nullptr; // Create a new pointer
-    vector<int>* indice_ptr = nullptr;
+    char *guess_ptr = nullptr; // Create a new pointer
+    vector<int> *indice_ptr = nullptr;
 
-    while (stop) {
+    while (stop)
+    {
         delete guess_ptr; // Clean up previous memory allocation (use delete[] for arrays)
         delete indice_ptr;
 
-        if (life >= 7) {
+        if (life >= 7)
+        {
             cout << "YOU HAVE LOST! The word was: " << play.goal << endl;
             break;
         }
 
-        else if (play.score >= (play.goal.size() - length_change)) {
+        else if (play.score >= (play.goal.size() - length_change))
+        {
             cout << "YOU HAVE WON! The word was: " << play.goal << endl;
             break;
         }
-        else {
+        else
+        {
             print_vector(play.letters);
             print_hangman(life);
             cout << "Word/Phrase: " << play.unknown << endl;
@@ -300,27 +334,27 @@ void start_game() {
 
             indice_ptr = check_guess(play, guess_ptr);
 
-            if (indice_ptr) {
+            if (indice_ptr)
+            {
                 int score = change_unknown(play, guess_ptr, indice_ptr); // No change in life
                 play.score = play.score + score;
             }
-            else { // Change in life
+            else
+            { // Change in life
                 delete indice_ptr;
                 life++;
             }
 
             remove_letters(play, guess_ptr);
-
         }
-
-
     }
 
     // delete guess_ptr; // Clean up guess_ptr
     cout << "Thanks for playing!" << endl;
 }
 
-int main() {
+int main()
+{
 
     start_game();
 
