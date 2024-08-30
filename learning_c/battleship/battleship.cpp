06afb6c84fe8ece9d853145c671fec66aeaf0c72
ImJudgement7497 <bmjm501@york.ac.uk>
2024-08-30 08:56:10 +0100

    save

--- learning_c/battleship/battleship.cpp
+++ learning_c/battleship/battleship.cpp
@@ -7,25 +7,50 @@
 
 using namespace std;
 
-class Ship {
+class Ship
+{
 
-    public:
-        int size;
-        vector<pair<int, int>> position;
-        int hits;
+public:
+    int size;                         // Size of the ship
+    vector<pair<int, int>> positions; // Vector of coordinates for position (needs to be same size as size)
+    int hits;                         // Number of hits on the ship
 
-        // Constructor
-        Ship(int size_value) : size(size_value) {};
+    // Constructor         Construct size value and default hits to 0
+    Ship(int size_value) : size(size_value), hits(0) {};
 
-        // Print ship size
-        void print_size() {
-            cout << size << endl;
+    // Check if size of ship is equal to the length of position vector
+    bool check_size() const
+    {
+        return size == positions.size();
+    }
+
+    // Set the poistion of the ship
+    void set_position(const vector<pair<int, int>> &pos)
+    {
+        positions = pos;
+    }
+
+    // Checks if a coordinate is equal to the position of ship
+    bool occupies_position(int x, int y) const
+    {
+        for (const auto &pos : positions)
+        {
+            if (pos.first == x && pos.second == y)
+            {
+                return true;
+            }
         }
+        return false;
+    }
 
+    // Checks if a ship is sunk
+    bool is_sunk() const
+    {
+        return hits >= size;
+    }
 };
 
 // class Player {
-
 
 // };
 
@@ -34,13 +59,12 @@ class Ship {
 // private:
 //     static const int grid_size = 10; // static ensures all functions within the class use the same object
 
-
 // };
 
-int main() {
+int main()
+{
 
     Ship ship(5);
-    ship.print_size();
+
     return 0;
-    
 }
