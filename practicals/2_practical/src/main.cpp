fa45faca241d1cf8785666221769572c0effd49d
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-10-11 18:34:38 +0100

    practical 3

--- practicals/2_practical/src/main.cpp
+++ practicals/2_practical/src/main.cpp
@@ -3,78 +3,149 @@
 #include <omp.h>
 #include <string>
 #include <vtimer_t.h>
+#include <cstdio>
 
 using namespace std;
 
-void question_1() {
+// void question_1()
+// {
+
+//     int my_thread;
+//     int total_threads;
+//     int sum = 0;
+//     int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+
+// #pragma omp parallel for schedule(static, 1) default(none) shared(sum1, sum2, sum3, sum4) private(my_thread, total_threads) reduction(+ : sum)
+//     for (int i = 1; i <= 100; i++)
+//     {
+//         sum += i;
+//         my_thread = omp_get_thread_num();
+//         total_threads = omp_get_num_threads();
+
+//         if (my_thread == 0)
+//         {
+//             sum1 += 1;
+//         }
+//         else if (my_thread == 1)
+//         {
+//             sum2 += 1;
+//         }
+//         else if (my_thread == 2)
+//         {
+//             sum3 += 1;
+//         }
+//         else
+//         {
+//             sum4 += 1;
+//         }
+//         string message = "Hello world from thread number " + to_string(my_thread) + " of " + to_string(total_threads);
+
+//         // std::cout << i << " " << message << std::endl;
+//         printf("%d %s \n", i, message.c_str());
+//     }
+//     printf("%d, %d, %d, %d, %d", sum, sum1, sum2, sum3, sum4);
+// }
+
+void question_1()
+{
 
     int my_thread;
     int total_threads;
+    int sum = 0;
+    int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
 
-    #pragma omp parallel private(my_thread, total_threads)
+#pragma omp parallel default(none) shared(sum1, sum2, sum3, sum4) private(my_thread, total_threads) reduction(task, + : sum)
     {
-        my_thread = omp_get_thread_num();
-        total_threads = omp_get_num_threads();
-        string message = "Hello world from thread number " + to_string(my_thread) + " of " + to_string(total_threads);
-    
-        cout << message << endl;
-    }
-}
 
-long question_2(const long N) {
-
-    long sum = 0;
-
-    for (int i = 1; i<=N; ++i) {
-
-        sum += i;
-    }
-
-    return sum;
-}
-
-pair<long, double> question_3(const long N) {
-
-    long sum = 0;
-    double total_time = 0;
-    int i;
-    double start = 0;
-    double end = 0;
-    pair<long, double> pair;
-
-    #pragma omp parallel private(i, start, end) reduction(+:total_time)
-    {
-        start = omp_get_wtime();
-        #pragma omp for private(i, start, end) reduction(+:sum)
-            for (i = 1; i<=N; ++i) {
+#pragma omp single
+        for (int i = 1; i <= 100; i++)
+        {
+#pragma omp task firstprivate(i) in_reduction(+ : sum)
+            {
                 sum += i;
+                my_thread = omp_get_thread_num();
+                total_threads = omp_get_num_threads();
+
+                if (my_thread == 0)
+                {
+                    sum1 += 1;
+                }
+                else if (my_thread == 1)
+                {
+                    sum2 += 1;
+                }
+                else if (my_thread == 2)
+                {
+                    sum3 += 1;
+                }
+                else
+                {
+                    sum4 += 1;
+                }
+                string message = "Hello world from thread number " + to_string(my_thread) + " of " + to_string(total_threads);
+
+                // std::cout << i << " " << message << std::endl;
+                printf("%d %s \n", i, message.c_str());
             }
-        end = omp_get_wtime();
-        total_time = end - start;
-    }   
+        }
 
-    pair.first = sum;
-    pair.second = total_time;
-
-    
-    return pair;
-
-
+        printf("%d, %d, %d, %d, %d \n", sum, sum1, sum2, sum3, sum4);
+    }
 }
+
+// long question_2(const long N) {
+
+//     long sum = 0;
+
+//     for (int i = 1; i<=N; ++i) {
+
+//         sum += i;
+//     }
+
+//     return sum;
+// }
+
+// pair<long, double> question_3(const long N) {
+
+//     long sum = 0;
+//     double total_time = 0;
+//     int i;
+//     double start = 0;
+//     double end = 0;
+//     pair<long, double> pair;
+
+//     #pragma omp parallel private(i, start, end) reduction(+:total_time)
+//     {
+//         start = omp_get_wtime();
+//         #pragma omp for private(i, start, end) reduction(+:sum)
+//             for (i = 1; i<=N; ++i) {
+//                 sum += i;
+//             }
+//         end = omp_get_wtime();
+//         total_time = end - start;
+//     }
+
+//     pair.first = sum;
+//     pair.second = total_time;
+
+//     return pair;
+
+// }
 
 int main()
 {
-    vtimer_t timer;
+    question_1();
+    // vtimer_t timer;
 
-    timer.start();
-    long N = 1000000;
+    // timer.start();
+    // long N = 1000000;
 
-    cout << "Serial: " << question_2(N) << endl;
-    timer.stop();
-    cout << "Serial Time: " << timer.elapsed_time() << endl;
+    // cout << "Serial: " << question_2(N) << endl;
+    // timer.stop();
+    // cout << "Serial Time: " << timer.elapsed_time() << endl;
 
-    pair<long, double> parra = question_3(N);
-    cout << "Parallel: " << parra.first << endl;
-    cout << "Parallel Time: " << parra.second << endl;
+    // pair<long, double> parra = question_3(N);
+    // cout << "Parallel: " << parra.first << endl;
+    // cout << "Parallel Time: " << parra.second << endl;
     return 0;
 }
--- practicals/3_practical/C/Mandelbrot/area.c
+++ practicals/3_practical/C/Mandelbrot/area.c
@@ -1,56 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <omp.h> 
+#include <omp.h>
 
-# define NPOINTS 2000
-# define MAXITER 2000
+#define NPOINTS 2000
+#define MAXITER 2000
 
-
-struct complex{
+struct complex
+{
   double real;
   double imag;
 };
 
-int main(){
-  int i, j, iter, numoutside = 0;
+int main()
+{
+  int numoutside = 0;
   double area, error, ztemp;
   struct complex z, c;
 
 /*
- *   
- *
- *     Outer loops run over npoints, initialise z=c
- *
- *     Inner loop has the iteration z=z*z+c, and threshold test
+ * Outer loops run over NPOINTS, initialize z = c
+ * Inner loop has the iteration z = z*z + c, and threshold test
  */
-  #pragma omp parallel for schedule(dynamic, 8) default(private) reduction(+:numoutside)
-  for (i=0; i<NPOINTS; i++) {
-    for (j=0; j<NPOINTS; j++) {
-      c.real = -2.0+2.5*(double)(i)/(double)(NPOINTS)+1.0e-7;
-      c.imag = 1.125*(double)(j)/(double)(NPOINTS)+1.0e-7;
-      z=c;
-      for (iter=0; iter<MAXITER; iter++){
-	ztemp=(z.real*z.real)-(z.imag*z.imag)+c.real;
-	z.imag=z.real*z.imag*2+c.imag; 
-	z.real=ztemp; 
-	if ((z.real*z.real+z.imag*z.imag)>4.0e0) {
-	  numoutside++; 
-	  break;
-	}
+#pragma omp parallel default(none) private(area, error, ztemp, z, c) reduction(task, + : numoutside)
+  {
+#pragma omp single
+
+    for (int i = 0; i < NPOINTS; i++)
+    {
+#pragma omp task firstprivate(i) in_reduction(+ : numoutside)
+      for (int j = 0; j < NPOINTS; j++)
+      {
+        c.real = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + 1.0e-7;
+        c.imag = 1.125 * (double)(j) / (double)(NPOINTS) + 1.0e-7;
+        z = c;
+
+        for (int iter = 0; iter < MAXITER; iter++)
+        {
+          ztemp = (z.real * z.real) - (z.imag * z.imag) + c.real;
+          z.imag = 2 * z.real * z.imag + c.imag;
+          z.real = ztemp;
+
+          if ((z.real * z.real + z.imag * z.imag) > 4.0)
+          {
+            numoutside++;
+            break;
+          }
+        }
       }
     }
   }
+  /*
+   * Calculate area and error and output the results
+   */
+  area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
+  error = area / (double)NPOINTS;
 
+  printf("Area of Mandelbrot set = %12.8f +/- %12.8f\n", area, error);
 
-
-/*
- *  Calculate area and error and output the results
- */
-
-      area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-      error=area/(double)NPOINTS;
-
-      printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
-
-  }
+  return 0;
+}
