258e9c412457faa821280d91e5a0f67c1d116c8f
ImJudgement7497 <bmjm501@york.ac.uk>
2024-10-14 09:30:16 +0100

    ppq 3 (still broken)

--- ppqs/3_ppq/src/main.cpp
+++ ppqs/3_ppq/src/main.cpp
@@ -7,6 +7,29 @@
 
 using namespace std;
 
+// int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+// void debugging_4_threads(int my_thread)
+// {
+
+//     if (my_thread == 0)
+//     {
+//         sum1 += 1;
+//     }
+//     else if (my_thread == 1)
+//     {
+//         sum2 += 1;
+//     }
+//     else if (my_thread == 2)
+//     {
+//         sum3 += 1;
+//     }
+//     else
+//     {
+//         sum4 += 1;
+//     }
+
+//     printf("%d, %d, %d, %d, %d \n", sum1, sum2, sum3, sum4);
+// }
 void serial()
 {
 
@@ -39,39 +62,66 @@ void serial()
 
 void parallel()
 {
+    // vector<int> N_values = {
+    //     1000, 2000, 5000,
+    //     10000, 20000, 50000,
+    //     100000, 200000, 500000,
+    //     1000000, 2000000, 5000000};
 
-    vector<int> N_values = {
-        1000, 2000, 5000,
-        10000, 20000, 50000,
-        100000, 200000, 500000,
-        1000000, 2000000, 5000000};
-
-    int my_thread = omp_get_thread_num();
+    vector<int> N_values = {5000000};
 
     for (int j = 0; j < N_values.size(); ++j)
     {
+        int my_thread;
+        int total_threads;
+        int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
         int N = N_values[j];
         vector<double> A(N, 0.0), B(N, 0.0);
-        double start_time = omp_get_wtime();
         double sum = 0;
-#pragma omp parallel for default(none) shared(A, B, N) reduction(+ : sum)
-        for (int i = 0; i < N; ++i)
+#pragma omp parallel default(none) shared(sum1, sum2, sum3, sum4, N, A, B) private(my_thread, total_threads) reduction(task, + : sum)
         {
-            A[i] = i + 1;
-            B[i] = A[i] * A[i];
-            A[i] = A[i] + sqrt(abs(sin(B[i]))) * 2.34;
-            sum += A[i];
-        }
-        double end_time = omp_get_wtime();
+#pragma omp single
+            for (int i = 0; i < N; ++i)
+#pragma omp task firstprivate(i) in_reduction(+ : sum)
+            {
+                A[i] = i + 1;
+                B[i] = A[i] * A[i];
+                A[i] = A[i] + sqrt(abs(sin(B[i]))) * 2.34;
+                sum += A[i];
 
-        string message = "N value: " + to_string(N) + " Sum: " + to_string(sum) + " Time: " + to_string(end_time - start_time);
+                // my_thread = omp_get_thread_num();
+                // total_threads = omp_get_num_threads();
+
+                // if (my_thread == 0)
+                // {
+                //     sum1 += 1;
+                // }
+                // else if (my_thread == 1)
+                // {
+                //     sum2 += 1;
+                // }
+                // else if (my_thread == 2)
+                // {
+                //     sum3 += 1;
+                // }
+                // else
+                // {
+                //     sum4 += 1;
+                // }
+            }
+            // printf("%d, %d, %d, %d \n", sum1, sum2, sum3, sum4);
+        }
+
+        string message = "N value: " + to_string(N) + " Sum: " + to_string(sum) + " Time: ";
         printf("%s \n", message.c_str());
     }
 }
 
 int main()
 {
+    // cout << "------------SERIAL----------------" << endl;
     // serial();
+    cout << "------------PARALLEL--------------" << endl;
     parallel();
     return 0;
 }
