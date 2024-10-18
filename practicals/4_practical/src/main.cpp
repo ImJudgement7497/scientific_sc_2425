60ae9cda25c3b73a5156dcdcfcc4fb9e1a53844a
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-10-18 14:33:30 +0100

    prac4

--- practicals/4_practical/src/main.cpp
+++ practicals/4_practical/src/main.cpp
@@ -6,16 +6,53 @@
 using namespace std;
 
 // Question 1 
+// int main(int argc, char **argv)
+// {
+
+//     int myrank, size;
+
+//     MPI_Init (&argc, &argv);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &myrank); // Get rank
+//     MPI_Comm_size(MPI_COMM_WORLD, &size); // Get size
+
+//     cout << "Hello World from rank " << myrank+1 << " of " << size << endl;
+
+//     MPI_Finalize ();
+
+//     exit (0);
+// }
+
+// Question 2
 int main(int argc, char **argv)
 {
 
-    int myrank, size;
+    int myrank, size, mydata;
+    MPI_Status status;
 
     MPI_Init (&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank); // Get rank
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Get size
 
-    cout << "Hello World from rank " << myrank+1 << " of " << size << endl;
+    mydata = myrank;
+
+    if (myrank == 0)
+    {
+        int ierr = MPI_Send(&mydata, 1, MPI_INT, 1, 178, MPI_COMM_WORLD);
+        cout << "Rank 0 sent: " << mydata << endl;
+        int ierr = MPI_Recv(&mydata, 1, MPI_INT, 1, 178, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        cout << "Rank 0 recieved: " << mydata << endl;
+    }
+
+    if (myrank == 1)
+    {   
+    
+        int ierr = MPI_Recv(&mydata, 1, MPI_INT, 0, 178, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        cout << "Rank 1 recieved: " << mydata << endl;
+        mydata = myrank;
+        int ierr = MPI_Send(&mydata, 1, MPI_INT, 0, 178, MPI_COMM_WORLD);
+        cout << "Rank 1 sent: " << mydata << endl;
+    }
+
 
     MPI_Finalize ();
 
