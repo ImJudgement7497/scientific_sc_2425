6daf257dccb85c340477da398b78c81da7caa8d0
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-10-30 22:30:28 +0000

    mini save

--- assignments/mini/src_parallel/main.cpp
+++ assignments/mini/src_parallel/main.cpp
@@ -1,6 +1,6 @@
 #include "serial.h" // Include the header file for access to global variables and functions
 
-int main()
+int main(&argc, &argv)
 {
     // Call the execute_serial function
     int result = XT;
