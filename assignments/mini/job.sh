893de27a29e492cc26abf174a409af1d0da75c62
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-11-06 21:06:00 +0000

    mini: changed job.sh

--- assignments/mini/job.sh
+++ assignments/mini/job.sh
@@ -1,31 +1,42 @@
 #!/bin/sh
-#SBATCH --job-name=MINI                # Job name
-#SBATCH --output=./logs/output.log           # Standard out and error log
-#SBATCH --mail-type=NONE                   # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=bmjm501@york.ac.uk         # NB change uid to your username if wanting to send mail
+#SBATCH --job-name=MINI                  # Job name
+#SBATCH --output=./logs/output.log       # Standard out and error log
+#SBATCH --mail-type=NONE                 # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bmjm501@york.ac.uk   # Change UID if you want to send mail
 
-#need ONE of the following two lines:
-#SBATCH --partition=teach         # priority queue for class work
-##SBATCH --account=dept-proj-year           # specify your project account if NOT doing class work
+# Need ONE of the following two lines:
+#SBATCH --partition=teach                # Priority queue for class work
+##SBATCH --account=dept-proj-year         # Specify project account if not doing class work
 
-#customise these according to job size and time required:
-#SBATCH --ntasks=10                         # Run 4 MPI tasks...
-#SBATCH --cpus-per-task=1                  # ...with each task using 1 core
-#SBATCH --time=00:05:00                    # Time limit hrs:min:sec
+# Customise these according to job size and time required:
+#SBATCH --cpus-per-task=1                # Each task uses 1 core
+#SBATCH --time=00:05:00                  # Time limit hrs:min:sec
 
-#actual executable info now:
+# Actual executable info now:
 EXEC="./bin/main"
 
-#tell user what is going on:
-echo My working directory is `pwd`
-echo Running job on host:
-echo -e '\t'`hostname` at `date`
-echo -e '\t'using $SLURM_NTASKS MPI tasks
-echo
+# Define a list of task counts to iterate over
+TASK_COUNTS=(1 2 4 5 10)  # List the desired numbers of tasks
 
-./runs/run_parallel.sh
-echo Executing
-mpiexec -n ${SLURM_NTASKS} $EXEC
+# Tell user what is going on
+echo "My working directory is $(pwd)"
+echo "Running job on host:"
+echo -e '\t' "$(hostname) at $(date)"
+
+# Loop through the task counts
+for NTASKS in "${TASK_COUNTS[@]}"; do
+    echo -e "\nRunning with ${NTASKS} tasks..."
+    
+    # Set SLURM_NTASKS for this run
+    export SLURM_NTASKS=$NTASKS
+    
+    # Execute the run script
+    ./runs/run_parallel.sh
+    echo "Executing mpiexec with ${SLURM_NTASKS} tasks"
+    mpiexec -n ${SLURM_NTASKS} $EXEC
+
+    echo -e "\nCompleted run with ${NTASKS} tasks at $(date)"
+done
 
 echo
-echo Job completed at `date`
+echo "All runs completed at $(date)"
