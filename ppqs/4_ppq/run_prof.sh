6fd4b983f9044432756b1f92e9cf426875a4381b
ImJudgement7497 <bmjm501@york.ac.uk>
2024-11-12 12:55:56 +0000

    ppq4: ran some tests using complier optimisation

--- ppqs/4_ppq/run_prof.sh
+++ ppqs/4_ppq/run_prof.sh
@@ -4,5 +4,5 @@ make clean
 make simple_md_C_pg
 ./simple_md_C_pg
 diff simple_md.log ./results/simple_md.log.ref
-gprof ./simple_md_C_pg >> ./results/change_4_profile.txt
+gprof ./simple_md_C_pg >> ./results/change_0_profile.txt
 make clean
