d8af1abb25db62519e5be014bc2ec87af13fad2d
ImJudgement7497 <bmjm501@york.ac.uk>
2024-11-13 11:07:30 +0000

    ppq4: ready for VIKING test

--- ppqs/4_ppq/run_prof.sh
+++ ppqs/4_ppq/run_prof.sh
@@ -4,5 +4,5 @@ make clean
 make simple_md_C_pg
 ./simple_md_C_pg
 diff simple_md.log ./results/simple_md.log.ref
-gprof ./simple_md_C_pg >> ./results/change_5_profile.txt
+gprof ./simple_md_C_pg >> ./results/viking_profile.txt
 make clean
