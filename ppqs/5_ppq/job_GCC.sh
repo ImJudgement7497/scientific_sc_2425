f6bfa21ccff06ec6bc8f69ba97594400bc3eb1dd
ImJudgement7497 <bmjm501@york.ac.uk>
2024-11-18 10:25:30 +0000

    ppq5: redo viking part c3

--- ppqs/5_ppq/job_GCC.sh
+++ ppqs/5_ppq/job_GCC.sh
@@ -27,8 +27,8 @@ echo
 
 module purge 
 module load GCC
-gfortran -O3 test_inv.f90
-./a.out >> ./results_c2/viking_GCC.txt
+gfortran -Ofast test_inv.f90
+./a.out >> ./results_c3/viking_GCC.txt
 rm ./a.out
 
 echo
