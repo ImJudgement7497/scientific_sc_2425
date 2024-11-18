3c1f12543781f8df7e2e8ee124b57aa0b1cef3f3
ImJudgement7497 <bmjm501@york.ac.uk>
2024-11-18 09:10:00 +0000

    ppq5: run scripts

---
+++ ppqs/5_ppq/GCC_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module purge 
+module load compiler/GCC
+gfortran -O0 test_inv.f90
+./a.out
---
+++ ppqs/5_ppq/INTEL_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module purge
+module load compiler/intel-compilers 
+ifort -O0 test_inv.f90
+./a.out
