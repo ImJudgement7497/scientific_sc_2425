4a3faccc03e573d057e11ec5bcd1b9f564658ef5
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-12-06 14:53:05 +0000

    prac10:  finalised

--- practicals/10_practical/src/main.cu
+++ practicals/10_practical/src/main.cu
@@ -12,11 +12,36 @@ void hello()
     return;
 }
 
+__global__
+void add(int N, float *A, float *B, float *C)
+{
+
+    int index = blockIdx.x * blockDim.x + threadId.x
+}
+
 int main()
 {
     // call kernel hello with 10 blocks, 10 threads per block
-    hello<<<5, 5>>>();
+    // hello<<<5, 5>>>();
     // ensure all computations are finished before exiting
+    
+    int N = 1000000
+    float *A, *B. *C
+
+    cudaMallocManaged(&A, N*sizeof(float))
+    cudaMallocManaged(&B, N*sizeof(float))
+    cudaMallocManaged(&C, N*sizeof(float))
+
+    for(int i = 0; i < N; i++) B[i] = 1.0f;
+    for(int i = 0; i < N; i++) C[i] = 2.0f;
+
+
+    
+    cudaFree(A)
+    cudaFree(B)
+    cudaFree(C)
+    
+    
     cudaDeviceSynchronize();
     // end main program with exit code 0 (all OK)
     return 0;
