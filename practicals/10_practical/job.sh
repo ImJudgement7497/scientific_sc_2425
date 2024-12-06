bfcee43a1a7ff99ccd824e7a1b0998f693c70f69
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-12-06 13:12:14 +0000

    prac10:  to VIKING

--- practicals/10_practical/job.sh
+++ practicals/10_practical/job.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
-#SBATCH --job-name=JOBNAME                # Job name
+#SBATCH --job-name=prac10                # Job name
 #SBATCH --output=./logs/JOBNAME_%j.log           # Standard out and error log
 #SBATCH --mail-type=NONE                   # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bmjm501@york.ac.uk         # NB change uid to your username if wanting to send mail
 
 #need ONE of the following two lines:
-#SBATCH --partition=teach         # priority queue for class work
-##SBATCH --account=dept-proj-year           # specify your project account if NOT doing class work
+#SBATCH --partition=gpu         # priority queue for class work
+#SBATCH --account=pet-teach-2024           # specify your project account if NOT doing class work
+#SBATCH --gres=gpu:1
 
 #customise these according to job size and time required:
-#SBATCH --ntasks=4                         # Run 4 MPI tasks...
+#SBATCH --ntasks=1                         # Run 4 MPI tasks...
 #SBATCH --cpus-per-task=1                  # ...with each task using 1 core
 #SBATCH --time=00:05:00                    # Time limit hrs:min:sec
 
-#actual executable info now:
-EXEC="./bin/main"
 
-#tell user what is going on:
-make clean
-make
-echo My working directory is `pwd`
-echo Running job on host:
-echo -e '\t'`hostname` at `date`
-echo -e '\t'using $SLURM_NTASKS MPI tasks
-echo
+module purge
+module load CUDA/12.2.2
 
-mpiexec -n ${SLURM_NTASKS} $EXEC
+nvcc ./src/main.cu -o ./bin/main.x
+nvidia-smi
+nsys nvprof ./bin/main.x
+
 
 echo
 echo Job completed at `date`
