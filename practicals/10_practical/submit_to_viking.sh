6291c636767ac6dc3faba6d842eaaf7d985170c8
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-12-06 14:03:33 +0000

    prac10:  to VIKING

--- practicals/10_practical/submit_to_viking.sh
+++ practicals/10_practical/submit_to_viking.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh viking 'cd scratch/scientific_sc_2425/practicals/10_practical && git pull && sbatch job.sh'
+ssh viking 'cd scratch/scientific_sc_2425/practicals/10_practical && git pull && sbatch job.sh && touch WORK.txt'
