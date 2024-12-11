ad618e3309b96136961000382cc18e98027e891e
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-12-11 15:36:11 +0000

    final: push to VIKING

--- assignments/final/Q4/job.sh
+++ assignments/final/Q4/job.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH --job-name=Q4_%j                # Job name
-#SBATCH --output=./logs/Q4.log           # Standard out and error log
+#SBATCH --job-name=Q4               # Job name
+#SBATCH --output=./logs/Q4_%j.log           # Standard out and error log
 #SBATCH --mail-type=ALL                   # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bmjm501@york.ac.uk         # NB change uid to your username if wanting to send mail
 
@@ -13,8 +13,8 @@
 #SBATCH --cpus-per-task=1                  # ...with each task using 1 core
 #SBATCH --time=00:20:00                    # Time limit hrs:min:sec
 
-
-bash ./run_serial.sh serial
-
+module load matplotlib/3.7.2-gfbf-2023a
+#bash ./run_serial.sh serial
+python3 graphing/graph.py
 echo
 echo Job completed at `date`
