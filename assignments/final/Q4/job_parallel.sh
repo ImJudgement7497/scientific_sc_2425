663612aa4f0e6abf4b8fc0c2406922d23cf4bc52
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-12-17 11:11:47 +0000

    final: push to VIKING

---
+++ assignments/final/Q4/job_parallel.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --job-name=Q4               # Job name
+#SBATCH --output=./logs/parallel_logs/Q4_%j.log           # Standard out and error log
+#SBATCH --mail-type=ALL                   # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bmjm501@york.ac.uk         # NB change uid to your username if wanting to send mail
+
+#need ONE of the following two lines:
+#SBATCH --partition=teach         # priority queue for class work
+##SBATCH --account=dept-proj-year           # specify your project account if NOT doing class work
+
+#customise these according to job size and time required:
+#SBATCH --ntasks=1                         # Run 4 MPI tasks...
+#SBATCH --cpus-per-task=48                  # ...with each task using 1 core
+#SBATCH --time=00:20:00                    # Time limit hrs:min:sec
+
+module load matplotlib/3.7.2-gfbf-2023a
+mkdir plots
+bash ./run_parallel.sh parallel
+echo
+echo Job completed at `date`
