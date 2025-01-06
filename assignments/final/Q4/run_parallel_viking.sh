2b32322c09d803afb92873d79a25ce69782220a7
BMJ Mason <bmjm501@login1.viking2.yor.alces.network>
2025-01-06 19:23:20 +0000

    final: Automated VIKING commit with results

--- assignments/final/Q4/run_parallel_viking.sh
+++ assignments/final/Q4/run_parallel_viking.sh
@@ -9,8 +9,8 @@ fi
 MAKE_RULE=$1
 
 # Define the fixed list of thread counts inside the script
-THREADS=("1" "8" "16" "24" "32" "40")
-#THREADS=("48" "56" "64" "72" "80" "88" "96")  # Add or remove thread numbers as needed
+#THREADS=("1" "8" "16" "24" "32" "40")
+THREADS=("48" "56" "64" "72" "80" "88" "96")  # Add or remove thread numbers as needed
 
 # Get L, r, and sampling frequency from config file
 L=$(grep -oP '^L\s*\K[0-9.]+$' ./config/config.txt)
