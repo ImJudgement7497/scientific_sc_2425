c0ad90f41f586e02f1ab091fcd2c334505ca5698
BMJ Mason <bmjm501@login2.viking2.yor.alces.network>
2025-01-07 11:01:14 +0000

    final: Automated VIKING commit with results

--- assignments/final/Q4/job_parallel.sh
+++ assignments/final/Q4/job_parallel.sh
@@ -13,7 +13,7 @@
 #SBATCH --cpus-per-task=96                  # ...with each task using 1 core
 #SBATCH --time=00:20:00                    # Time limit hrs:min:sec
 
-bash ./run_parallel_viking.sh parallel_vik
+bash ./run_parallel_viking.sh parallel
 
 echo
 echo Job completed at `date`
