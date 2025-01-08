ce89805b2f18e20cff59e39c30739a4c0494219e
ImJudgement7497 <benwalshe.mason@outlook.com>
2025-01-08 16:47:54 +0000

    final: ready for scaling tests

--- assignments/final/Q3/run_parallel_viking_4.sh
+++ assignments/final/Q3/run_parallel_viking_4.sh
@@ -6,7 +6,7 @@ for NUM_THREADS in "${THREADS[@]}"; do
     export OMP_NUM_THREADS=$NUM_THREADS  # Set number of threads
 
     echo "Running with $NUM_THREADS threads..."
-    ./bin/main_packing_parallel
+    ./main_packing_parallel
     diff first_run/final_positions.txt final_positions.txt
     echo "Differences above (if any)"
     echo "-----------------------------------"
