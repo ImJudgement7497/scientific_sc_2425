72d2a7c3229b6566f0d3875accaecce1bd797ac8
ImJudgement7497 <benwalshe.mason@outlook.com>
2025-01-08 14:15:32 +0000

    final: Q3 test on viking

---
+++ assignments/final/Q3/job_parallel.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH --job-name=Q4               # Job name
+#SBATCH --output=./logs/parallel_logs/Q3_%j.log           # Standard out and error log
+#SBATCH --mail-type=ALL                   # Specify when to mail (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bmjm501@york.ac.uk         # NB change uid to your username if wanting to send mail
+
+#need ONE of the following two lines:
+#SBATCH --partition=teach         # priority queue for class work
+##SBATCH --account=dept-proj-year           # specify your project account if NOT doing class work
+
+#customise these according to job size and time required:
+#SBATCH --ntasks=1                         # Run 4 MPI tasks...
+#SBATCH --cpus-per-task=96                  # ...with each task using 1 core
+#SBATCH --time=00:20:00                    # Time limit hrs:min:sec
+
+g++ -pg -fopenmp packing/packing_edited.cpp -o main_packing_parallel
+export OMP_NUM_THREADS=96
+./main_packing_parallel
+
+echo
+echo Job completed at `date`
